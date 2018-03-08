3a0ff08799b0ec00625cf6312a10a1c044e4eb59
kicky13 <kicky120@gmail.com>
2018-03-09 02:39:00 +0700

    New

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,26 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/logout', 'Auth\LoginController@logout');
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/personal', 'PesertaController@index');
+Route::get('/personal/create', 'PesertaController@createView');
+Route::post('/personal/create', 'PesertaController@create');
+Route::get('/personal/update/personal', 'PesertaController@viewUpdatePersonal');
+Route::post('/personal/update/personal', 'PesertaController@updatePersonal');
+Route::get('/personal/update/parent', 'PesertaController@viewUpdateParent');
+Route::post('/personal/update/parent', 'PesertaController@updateParent');
+
+Route::get('/prestasi', 'PrestasiController@index');
+Route::get('/prestasi/create', 'PrestasiController@createView');
+Route::post('/prestasi/create', 'PrestasiController@create');
+Route::get('/prestasi/download/{id}', 'PrestasiController@download');
+Route::get('/prestasi/delete/{id}', 'PrestasiController@delete');
+
+Route::get('/organisasi', 'OrganisasiController@index');
+Route::get('/organisasi/create', 'OrganisasiController@createView');
+Route::post('/organisasi/create', 'OrganisasiController@create');
+Route::get('/organisasi/download/{id}', 'OrganisasiController@download');
+Route::get('/organisasi/delete/{id}', 'OrganisasiController@delete');
